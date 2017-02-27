4045826920c59c08c3132edff35dc7838eac7244
Пиванов Роман <pivanov.roman@gmail.com>
2017-02-27 19:45:20 +0200

    main
    first load

---
+++ src/Berkeley.java
@@ -0,0 +1,60 @@
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.Database;
+import com.sleepycat.je.DatabaseException;
+import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.DatabaseConfig;
+
+import java.io.File;
+
+
+/**
+ * Created by RPivanov on 27.02.2017.
+ */
+public class Berkeley {
+
+    private Environment env = null;
+    private Database db = null;
+    private File fil = null;
+
+    public void open() {
+        try {
+            EnvironmentConfig conf = new EnvironmentConfig();
+            conf.setAllowCreate(true);
+
+            fil = new File("/export/env");
+            if (fil.exists()){
+                fil.mkdir();
+            }
+
+            env = new Environment(fil, conf);
+        }
+        catch (DatabaseException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        DatabaseConfig conf = new DatabaseConfig();
+        conf.setAllowCreate(true);
+        db = env.openDatabase(null,"sample",conf);
+
+        //
+
+        //
+        db.sync();
+    }
+
+    public void close() {
+        try {
+            if (db != null) {
+                db.close();
+            }
+            if (env != null) {
+                env.close();
+            }
+        } catch (DatabaseException dbe) {
+        // Exception handling goes here
+        }
+
+    }
+
+
+}
---
+++ src/test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by RPivanov on 27.02.2017.
+ */
+public class test {
+    public static void main(String[] args){
+        Berkeley brk = new Berkeley();
+        brk.open();
+    }
+}
